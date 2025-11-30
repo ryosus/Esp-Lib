9bddf23bdea703aeedaadf9e74cf52b24dfbefea
ryosus <ryo.guo@outlook.com>
2025-11-30 21:38:18 +0800

    fixed fnaf en item esp

--- examples/fnaf eternal nights item esp.lua
+++ examples/fnaf eternal nights item esp.lua
@@ -1,37 +1,35 @@
 -- // esp-lib.lua
 local esplib = loadstring(game:HttpGet("https://raw.githubusercontent.com/ryosus/Esp-Lib/refs/heads/main/esp_lib.lua"))()
 
--- // Find existing tools in workspace
-for i, v in pairs(workspace:GetChildren()) do
-    if v:IsA("Tool") then
-    esplib.add_box(v.Handle)
-    esplib.add_name(v.Handle, v.Name)
-    esplib.add_distance(v.Handle)
-    elseif v.Name in ["Battery", "OldFlashlight"] then
+function check(v)
+    if v:IsA("Tool") or v.Name == "OldFlashlight" then
+        esplib.add_box(v.Handle)
+        esplib.add_name(v.Handle, v.Name)
+        esplib.add_distance(v.Handle)
+    elseif v.Name == "Battery" then
         esplib.add_box(v)
         esplib.add_name(v)
         esplib.add_distance(v)
     end
 end
 
+-- // Find existing tools in workspace
+for i, v in pairs(workspace:GetChildren()) do
+    check(v)
+end
+
 -- // Monitor for new tools being added to workspace
-workspace.ChildAdded:Connect(function(child)
-    if child:IsA("Tool") then
-        esplib.add_box(child.Handle)
-        esplib.add_name(child.Handle, child.Name)
-        esplib.add_distance(child.Handle)
-    elseif child.Name in ["Battery", "OldFlashlight"] then
-        esplib.add_box(child)
-        esplib.add_name(child)
-        esplib.add_distance(child)
-    end
+workspace.ChildAdded:Connect(function(v)
+    check(v)
 end)
 
 -- check if items are being picked up and remove esp
 game:GetService("RunService").Stepped:Connect(function()
-    for i, v in pairs(esplib.get_esp_instances()) do
-        if i.Parent ~= workspace then
-            esplib.remove_esp(i)
-        end
+    for i, v in pairs(getgenv().espinstances) do
+        pcall(function()
+            if i.Parent.Parent ~= workspace and i.Name ~= "Battery" then
+                esplib.remove_esp(i)
+            end
+        end)
     end
 end)
